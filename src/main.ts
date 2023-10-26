5ee0b4202797bd8f1a5306523b56b99d794892b0
Benthan Vu <bxvu@ucsc.edu>
2023-10-25 18:25:44 -0700

    done pair development

--- src/main.ts
+++ src/main.ts
@@ -21,6 +21,14 @@ const imageUrls = [
     "/tile8.png"
 ];
 
+let tiles: HTMLImageElement[] = []
+
+imageUrls.forEach(url => {
+    const i = new Image();
+    i.src = url;
+    tiles.push(i);
+});
+
 
 //defining the size of the main grid
 const numTiles = 32;
@@ -31,59 +39,19 @@ const tileSize = gridCanvas.width / numTiles;
 const numSelectables = imageUrls.length;
 const selectHeight = selectCanvas.height / numSelectables;
 
-interface refImage {
-    imageReference: HTMLImageElement;
-}
-
-let tiles =  {
-    tile1: {
-        imagePath: "/tile1.png",
-        imageReference: new Image()
-    },
-    tile2: {
-        imagePath: "/tile2.png",
-        imageReference: new Image()
-    },
-    tile3: {
-        imagePath: "/tile3.png",
-        imageReference: new Image()
-    },
-    tile4: {
-        imagePath: "/tile4.png",
-        imageReference: new Image()
-    },
-    tile5: {
-        imagePath: "/tile5.png",
-        imageReference: new Image()
-    },
-    tile6: {
-        imagePath: "/tile6.png",
-        imageReference: new Image()
-    },
-    tile7: {
-        imagePath: "/tile7.png",
-        imageReference: new Image()
-    },
-    tile8: {
-        imagePath: "/tile8.png",
-        imageReference: new Image()
-    }
-}
-
-
 //creating the tilemap nested array
-let tilemap: refImage[][] = new Array(numTiles);
+let tilemap: number[][] = new Array(numTiles);
 
 for(let i = 0; i < numTiles; i++) {
     let row = new Array(numTiles);
     for (let j = 0; j < numTiles; j++) {
-        row[j] = tiles.tile1.imageReference
+        row[j] = 0;
     }
     tilemap[i] = row;
 }
 
 //track the selected tile
-let currentTile = "/tile1.png";
+let currentTile = 0;
 
 //draw the initial canvases
 redrawTilemap();
@@ -91,11 +59,11 @@ drawSelectCanvas();
 
 
 //Function that draws a texture to a specific canvas ctx
-function drawTexture(row: number, col: number, ctx: CanvasRenderingContext2D, image: HTMLImageElement, width: number, height: number, cellSize: number) {
-    // image.onload = () => {
-    //     ctx.drawImage(image, row * cellSize, col * cellSize, width, height)
-    // };
-    ctx.drawImage(image, row * cellSize, col * cellSize, width, height)
+function drawTexture(row: number, col: number, ctx: CanvasRenderingContext2D, image: number, width: number, height: number, cellSize: number) {
+    tiles[image].onload = () => {
+        ctx.drawImage(tiles[image], row * cellSize, col * cellSize, width, height)
+    };
+    ctx.drawImage(tiles[image], row * cellSize, col * cellSize, width, height)
 }
 
 
@@ -106,7 +74,7 @@ function redrawTilemap()
   gridCtx.clearRect(0, 0, gridCanvas.width, gridCanvas.height);
     for (let i = 0; i < numTiles; i++) {
         for (let j = 0; j < numTiles; j++) {
-            drawTexture(i, j, gridCtx, tilemap[i][j].imageReference, gridCanvas.width / numTiles, gridCanvas.height / numTiles, tileSize);
+            drawTexture(i, j, gridCtx, tilemap[i][j], gridCanvas.width / numTiles, gridCanvas.height / numTiles, tileSize);
         }
     }
 }
@@ -116,7 +84,7 @@ gridCanvas.addEventListener("click", (e) => {
     const coordX = Math.trunc(e.offsetX / tileSize);
     const coordY = Math.trunc(e.offsetY / tileSize);
 
-    tilemap[coordX][coordY].imageReference.src = currentTile;
+    tilemap[coordX][coordY] = currentTile;
     redrawTilemap();
 })
 
@@ -127,13 +95,12 @@ gridCanvas.addEventListener("click", (e) => {
 function drawSelectCanvas()
 {
     for (let i = 0; i < numSelectables; i++) {
-        const selectableImage = new Image();
-        selectableImage.src = imageUrls[i];
-        drawTexture(0, i, selectCtx, selectableImage, selectCanvas.width, selectHeight, 64);
+        drawTexture(0, i, selectCtx, i, selectCanvas.width, selectHeight, 64);
     }
 }
 
 selectCanvas.addEventListener("click", (e) => {
     const coordY = Math.trunc(e.offsetY / selectHeight);
-    currentTile = imageUrls[coordY];
+    currentTile = coordY;
+    console.log(coordY)
 })
